214ea4ae33e0b7246671fcfb12b0f6e098b48a44
daniel <dachival@correo.ugr.es>
2024-07-15 08:55:08 +0200

    a

--- fish/config.fish
+++ fish/config.fish
@@ -60,12 +60,71 @@ alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 
-# Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
-alias l.='exa -a | egrep "^\."'
+
+# # Changing "ls" to "exa"
+# alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+# alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+# alias ll='exa -l --color=always --group-directories-first'  # long format
+# alias lt='exa -aT --color=always --group-directories-first' # tree listing
+# alias l.='exa -a | egrep "^\."'
+
+
+
+
+# Alias LS
+
+alias ll='exa --icons -lh --group-directories-first'
+alias la='exa --icons -lgha --group-directories-first'
+alias l='exa --icons --group-directories-first'
+alias lla='exa --icons -lha --group-directories-first'
+alias ls='exa --icons --group-directories-first'
+alias lt='exa --icons --tree'
+alias lta='exa -lgha --icons --tree'
+alias lld='exa --icons -lh -s modified'
+alias llad='exa --icons -lha -s modified'
+
+
+alias cat='batcat'
+alias icat="kitty +kitten icat"
+# alias remove_mysql_root='/home/dachival/Proyectos/Scripts/rm_mysql.sh'
+
+
+alias python='/usr/bin/python3'
+alias vim="/home/daniel/.appimage/neovim.appimage"
+alias emacs="macsclient -c -a 'emacs'"
+
+alias grub-update='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+
+#git
+
+alias gitgraph="git log --all --decorate --oneline --graph"
+alias gitupdate="git fetch . && git pull"
+alias gitstat="git status"
+alias gitad="git add . "
+alias gitcom="git commit -m"
+
+
+#Refresh
+# alias refresh="source $HOME/.zshrc "
+
+#Convert Video
+alias convi="ffmpeg -i $1 -c:v libx264 -crf 25 $2"
+
+#launch android emulator
+alias emulator="$HOME/Android/Sdk/emulator/emulator"
+
+
+
+# fzf
+alias preview="fzf --preview='batcat --color=always --style=numbers --theme OneHalfDark {}' --preview-window=down"
+
 
 ### SETTING THE STARSHIP PROMPT ###
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/daniel/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
