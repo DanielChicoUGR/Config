b4c8ca952611998bb1f3ff554de22601707ad95d
daniel <dachival@correo.ugr.es>
2024-08-29 09:52:50 +0200

    pyinstaller github action hotfix

--- fish/config.fish
+++ fish/config.fish
@@ -128,3 +128,17 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/daniel/anaconda3/bin/conda
+    eval /home/daniel/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/daniel/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/home/daniel/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/daniel/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
