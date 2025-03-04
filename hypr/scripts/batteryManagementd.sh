2c9c3e85693ea901c07cb559912ea403567be35b
Daniel <dachival@correo.ugr.es>
2025-03-04 14:49:30 +0100

    woofi scripts instalados y funcionando. Modificaciones sobre archivos de configuración (show-keybinds no funciona)

--- hypr/scripts/batteryManagementd.sh
+++ hypr/scripts/batteryManagementd.sh
@@ -5,29 +5,29 @@ handle_battery_status_change() {
     while true; do
         status=$(cat /sys/class/power_supply/BAT1/status)
         case "$status" in
-            "Charging")
-                echo "Cargador conectado" >> /tmp/battery.log
-                if [ -f /tmp/swayidle.pid ]; then
-                    kill $(cat /tmp/swayidle.pid)
-                    rm /tmp/swayidle.pid
-                fi
-                # Aquí puedes agregar las acciones que deseas realizar cuando el cargador está conectado
-                ;;
-            "Discharging")
-                echo "Cargador desconectado" >> /tmp/battery.log
-                # Aquí puedes agregar las acciones que deseas realizar cuando el cargador está desconectado
-                if [ ! -f /tmp/swayidle.pid ]; then
-                    swayidle -w timeout 10 'systemctl suspend' &
-                    echo $! > /tmp/swayidle.pid
-                fi
-                ;;
-            "Full")
-                echo "Batería completamente cargada" >> /tmp/battery.log
-                # Aquí puedes agregar las acciones que deseas realizar cuando la batería está completamente cargada
-                ;;
-            *)
-                echo "Estado de la batería desconocido: $status" >> /tmp/battery.log
-                ;;
+        "Charging")
+            echo "Cargador conectado" >>/tmp/battery.log
+            if [ -f /tmp/swayidle.pid ]; then
+                kill $(cat /tmp/swayidle.pid)
+                rm /tmp/swayidle.pid
+            fi
+            # Aquí puedes agregar las acciones que deseas realizar cuando el cargador está conectado
+            ;;
+        "Discharging")
+            echo "Cargador desconectado" >>/tmp/battery.log
+            # Aquí puedes agregar las acciones que deseas realizar cuando el cargador está desconectado
+            if [ ! -f /tmp/swayidle.pid ]; then
+                swayidle -w timeout 600 'systemctl suspend' &
+                echo $! >/tmp/swayidle.pid
+            fi
+            ;;
+        "Full")
+            echo "Batería completamente cargada" >>/tmp/battery.log
+            # Aquí puedes agregar las acciones que deseas realizar cuando la batería está completamente cargada
+            ;;
+        *)
+            echo "Estado de la batería desconocido: $status" >>/tmp/battery.log
+            ;;
         esac
         sleep 60s
     done
